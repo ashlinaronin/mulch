d2373093f3077cdc3248e29f26947262d6442e65
ashlinaronin <ashlinaronin@gmail.com>
2021-01-16 18:17:35 -0800

    Add burpee circle on a weird cliff

---
+++ Assets/Scripts/PedalBike.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedalBike : MonoBehaviour
+{
+    public float speed = 100.0f;
+
+    void Update()
+    {
+        this.transform.Rotate(0, 0, -Time.deltaTime * speed);
+    }
+}
