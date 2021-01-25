84150003a3585220972bd80bf3a673c02366e0ef
ashlinaronin <ashlinaronin@gmail.com>
2021-01-24 21:40:30 -0800

    Trigger pew sound when passing through waypoint

---
+++ Assets/TriggerSound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerSound : MonoBehaviour
+{
+
+    AudioSource source;
+
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        source.Play();
+    }
+}
