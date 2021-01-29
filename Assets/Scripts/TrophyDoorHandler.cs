d806ff9989b745d81a043865c359eedf881cc642
ashlinaronin <ashlinaronin@gmail.com>
2021-01-28 22:28:57 -0800

    Add starter script for trophy door

---
+++ Assets/Scripts/TrophyDoorHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrophyDoorHandler : MonoBehaviour
+{
+
+    private Animator animator = null;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        animator.SetBool("isOpen", true);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        animator.SetBool("isClosed", false);
+    }
+}
