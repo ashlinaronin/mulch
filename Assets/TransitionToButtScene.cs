d0d9c785ed4019e66304a4b95cc9fb7e00b533e7
ashlinaronin <ashlinaronin@gmail.com>
2021-01-29 21:57:12 -0800

    Butt door transition to butt scene

---
+++ Assets/TransitionToButtScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionToButtScene : MonoBehaviour
+{
+    // todo: make generic?
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.parent.CompareTag("Player"))
+        {
+            StartCoroutine(LoadButtSceneAsync());
+        }
+    }
+
+    IEnumerator LoadButtSceneAsync()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Scenes/InsideButtScene");
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
