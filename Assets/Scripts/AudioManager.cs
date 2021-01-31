08773661019109278fd1d323f8ff15bb2e08e4da
ashlinaronin <ashlinaronin@gmail.com>
2021-01-30 21:49:51 -0800

    Add music transition when beginning ascent

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioSource backgroundMusic;
+
+    public void ChangeBackgroundMusic(AudioClip music)
+    {
+        if (backgroundMusic.clip.name == music.name) return;
+
+        backgroundMusic.Stop();
+        backgroundMusic.clip = music;
+        backgroundMusic.Play();
+    }
+}
---
+++ Assets/Scripts/SwitchMusicTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMusicTrigger : MonoBehaviour
+{
+    public AudioClip newTrack;
+    private AudioManager audioManager;
+
+    void Start()
+    {
+        audioManager = FindObjectOfType<AudioManager>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (newTrack == null) return;
+
+        if (other.transform.parent.CompareTag("Player"))
+        {
+            audioManager.ChangeBackgroundMusic(newTrack);
+        }
+    }
+}
