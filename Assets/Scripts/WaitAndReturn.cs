168ba5e85ff6c387c576121d6c5a283294fddd16
ashlinaronin <ashlinaronin@gmail.com>
2021-04-28 21:40:17 -0700

    Switch to mac build target; wait 5 seconds when finishing then restart

---
+++ Assets/Scripts/WaitAndReturn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WaitAndReturn : MonoBehaviour
+{
+    public string returnSceneName;
+    public float waitSeconds = 5;
+
+    void Start()
+    {
+        StartCoroutine(WaitAndRestartCoroutine());
+    }
+
+    IEnumerator WaitAndRestartCoroutine()
+    {
+        yield return new WaitForSeconds(waitSeconds);
+        SceneManager.LoadScene(returnSceneName);
+    }
+}
